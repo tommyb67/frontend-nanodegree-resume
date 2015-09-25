9f16f6ddacba36a979156cdcfbb2343b3a0e6ffc
Tom Brennan <tommybrennan@gmail.com>
2015-09-25 19:07:01 -0400

    set the name and role in the header of the resume

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,1 +1,19 @@
+/*
 $("#main").append(["Tommy Brennan"]);
+var awesomeThoughts = "I am Tom and I am Awesome!"
+console.log(awesomeThoughts);
+var funThoughts = awesomeThoughts.replace("Awesome", "Fun");
+console.log(funThoughts);
+$("#main").append([funThoughts]);
+*/
+
+var name = "Tommy Brennan";
+var formattedName = HTMLheaderName.replace("%data%", name);
+
+var role = "Front End Web Developer";
+var formattedRole = HTMLheaderRole.replace("%data%", role);
+
+$('#header').append(formattedName);
+$('#header').append(formattedRole);
+
+
