1706e3adb3ca5714b096a3a7d4c599cbdc946c61
Tom Brennan <tommybrennan@gmail.com>
2015-10-03 15:50:28 -0400

    updated to resumebuilder js file

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,44 +1,17 @@
-var skills = ["HTML", "CSS", "JavaScript", "Ruby"];
-
 var bio  = {
   "name": "Tommy Brennan",
   "role": "Full Stack Web Developer",
   "contacts": {
     "mobile": "718-440-4306",
     "email": "tommybrennan@gmail.com",
-    "github": "",
-    "linkedIn": ""
+    "github": "https://github.com/tommyb67",
+    "linkedIn": "https://www.linkedin.com/in/tommybrennan1"
   },
   "welcomeMessage": "lorum ipsum lorum ispum lorum ipsum",
-  "skills": skills,
-  "bioPic": "images/headshot.jpg",
-}
-
-var work = {
-  "jobs": [
-  {
-    "employer": "Credit Suisse",
-    "roles":
-    {
-      "name": "Americas Operations",
-      "description": "COO Group"
-    },
-    "location": "New York",
-    "datesWorked": "Aug-2002/Dec-2012"
-  },
-  {
-    "name": "General Assembly",
-    "city": "New York",
-    "degree": "Certificate",
-    "major": "Full Stack Immersive Web Development"
-  },
-  {
-    "name": "Udacity",
-    "city": "Online",
-    "degree": "Nanodegree",
-    "major": "Front End Web Developer"
-  }
- ]
+  "skills": [
+    "HTML", "CSS", "JavaScript", "Ruby"
+  ],
+  "bioPic": "images/headshot.jpg"
 }
 
 var education = {
@@ -48,115 +21,50 @@ var education = {
     "city": "Cortland",
     "degree": "BS",
     "major": "Management Science/Economics"
-  },
+    "url": "http://www2.cortland.edu/home/"
+    }
+  ],
+  "onlineCourses": [
   {
-    "name": "General Assembly",
+    "title": "Full Stack Immersive Web Development"
+    "school": "General Assembly",
     "city": "New York",
     "degree": "Certificate",
-    "major": "Full Stack Immersive Web Development"
+    "url": "Full Stack Immersive Web Development"
   },
   {
-    "name": "Udacity",
+    "title": "Front-End Web Developer"
+    "school": "Udacity",
     "city": "Online",
     "degree": "Nanodegree",
-    "major": "Front End Web Developer"
+    "url": "https://www.udacity.com/course/front-end-web-developer-nanodegree"
   }
+ ]
+}
+
+var work = {
+  "jobs": [
+  {
+    "employer": "Credit Suisse",
+    "title": "AVP"
+    {
+      "name": "Americas Operations",
+      "description": "COO Group"
+    },
+    "location": "New York",
+    "datesWorked": "Aug-2002/Dec-2012"
+  },
  ]
 }
 
 var projects = {
   "projects": [
   {
-    "name": "State University of New York, College at Cortland",
-    "date": "August 2014",
-    "description": "Udacity Projects",
+    "title": "Online Portfolio",
+    "date": "September 2015",
+    "description": "Udacity Project",
     "image": "images/placeholder.jpg"
+    "url": "http://tommyb67.github.io/udacity-portfolio-project/"
   },
-  {
-    "name": "General Assembly",
-    "city": "New York",
-    "degree": "Certificate",
-    "major": "Full Stack Immersive Web Development"
-  },
-  {
-    "name": "Udacity",
-    "city": "Online",
-    "degree": "Nanodegree",
-    "major": "Front End Web Developer"
-  }
  ]
 }
-
-if (bio.skills.length > 0) {
-
-  $("#header").append(HTMLskillsStart);
-
-  var formattedSkill = HTMLskills.replace("%data%", bio.skills[0]);
-  $("#skills").append(formattedSkill);
-
-  formattedSkill = HTMLskills.replace("%data%", bio.skills[1])
-  $("#skills").append(formattedSkill);
-
-  formattedSkill = HTMLskills.replace("%data%", bio.skills[2])
-  $("#skills").append(formattedSkill);
-
-  formattedSkill = HTMLskills.replace("%data%", bio.skills[3])
-  $("#skills").append(formattedSkill);
-
-}
-
-//$('#main').append(schools.name);
-
-
-// This file should only contain objects for:
-// bio
-// work
-// education
-// projects
-
-
-
-var name = "Tommy Brennan";
-var formattedName = HTMLheaderName.replace("%data%", name);
-
-var role = "Front End Web Developer";
-var formattedRole = HTMLheaderRole.replace("%data%", role);
-
-$('#header').append(formattedName);
-$('#header').append(formattedRole);
-
-
-// $("#main").append(["Tommy Brennan"]);
-// var awesomeThoughts = "I am Tom and I am Awesome!"
-// console.log(awesomeThoughts);
-// var funThoughts = awesomeThoughts.replace("Awesome", "Fun");
-// console.log(funThoughts);
-// $("#main").append([funThoughts]);
-
-
-// $('#main').append(bio.name);
-// $('#main').append(bio.role);
-// $('#main').append(bio.phone);
-// $('#main').append(bio.email);
-// $('#main').append(bio.image);
-// $('#main').append(bio.message);
-// $('#main').append(bio.skills);
-
-// var currentWork = {};
-// currentWork.name = "Canon";
-// currentWork.address = "One Canon Drive, Melville, NY";
-// currentWork.startDate = "August 2015";
-
-// $('#main').append(currentWork.name);
-// $('#main').append(currentWork.address);
-// $('#main').append(currentWork.startDate);
-
-
-
-
-
-
-
-
-
-
